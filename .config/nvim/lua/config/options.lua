72d25540816ed4dd67949355ca139b33c809713f
Soroush Sherafat <sorousherafat@gmail.com>
2025-02-27 14:56:40 +0330

    added bidi text support to `nvim` now that we are using `kitty`

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local g = vim.g
+local o = vim.opt
 
 g.autoformat = false
+
+o.termbidi = true
